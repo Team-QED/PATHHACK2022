de1e0bb2f9bdb57402825127afdb6d55ad3d6212
UPs <42921982+Up-s@users.noreply.github.com>
2022-04-30 03:50:59 +0900

    UPDATE

--- PathHack/Splash/SplashViewController.swift
+++ PathHack/Splash/SplashViewController.swift
@@ -11,8 +11,33 @@ import SnapKit
 
 class SplashViewController: UIViewController {
     
-    private let logoImageView = UIImageView().then {
-        $0.contentMode = .scaleAspectFit
+    private lazy var logoImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.image = UIImage(named: "splash")
+        let blurEffect = UIBlurEffect(style: .regular)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+        visualEffectView.alpha = 0.6
+        visualEffectView.frame = self.view.frame
+        $0.addSubview(visualEffectView)
+    }
+    
+    private let titleView = UIView().then {
+        $0.backgroundColor = UIColor.appColor(.darkGreenColor).withAlphaComponent(0.95)
+        $0.layer.cornerRadius = 6
+    }
+    
+    private let titleLabel = UILabel().then {
+        $0.text = "PINNED"
+        $0.textColor = UIColor.appColor(.daisyColor)
+        $0.font = UIFont.systemFont(ofSize: 50, weight: .bold)
+        $0.backgroundColor = .clear
+    }
+    
+    private let subLabel = UILabel().then {
+        $0.text = "QED"
+        $0.textColor = UIColor.white
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        $0.backgroundColor = .clear
     }
     
     override func loadView() {
@@ -39,15 +64,32 @@ class SplashViewController: UIViewController {
     private func attribute() {
         view.backgroundColor = .white
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
-            self.showMainVC()
-        })
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 5, execute: {
+//            self.showMainVC()
+//        })
     }
 
     private func setupUI() {
         view.addSubview(logoImageView)
         logoImageView.snp.makeConstraints {
-            $0.center.equalToSuperview()
+            $0.edges.equalToSuperview()
+        }
+        
+        view.addSubview(titleView)
+        titleView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview().offset(-96)
+            make.leading.trailing.equalToSuperview().inset(70)
+            make.height.equalTo(titleView.snp.width)
+        }
+        
+        titleView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        titleView.addSubview(subLabel)
+        subLabel.snp.makeConstraints { make in
+            make.trailing.bottom.equalToSuperview().inset(12)
         }
     }
 }
