6ccb88af0662e57600ef37a4c39ae868de56d45c
furydeveloper <furysecu@gmail.com>
2022-04-30 01:43:13 +0900

    Record 추가

--- PathHack/Main/MainViewController.swift
+++ PathHack/Main/MainViewController.swift
@@ -10,12 +10,12 @@ import SnapKit
 
 class MainViewController: UIViewController {
     
-    private let badges: [Badge] = [
-//        Badge(name: "뱃지1", level: <#T##Badge.Level#>, currRecord: <#T##Double#>,  percent: 55, imageName: "profile"),
-//        Badge(name: "뱃지2", percent: 5, imageName: "profile"),
-//        Badge(name: "뱃지3", percent: 24, imageName: "profile"),
-//        Badge(name: "뱃지4", percent: 60, imageName: "profile"),
-//        Badge(name: "뱃지5", percent: 40, imageName: "profile"),
+    private var badges: [Badge] = [
+        //        Badge(name: "뱃지1", level: <#T##Badge.Level#>, currRecord: <#T##Double#>,  percent: 55, imageName: "profile"),
+        //        Badge(name: "뱃지2", percent: 5, imageName: "profile"),
+        //        Badge(name: "뱃지3", percent: 24, imageName: "profile"),
+        //        Badge(name: "뱃지4", percent: 60, imageName: "profile"),
+        //        Badge(name: "뱃지5", percent: 40, imageName: "profile"),
     ]
     
     private lazy var scrollView = UIScrollView().then {
@@ -235,10 +235,10 @@ class MainViewController: UIViewController {
         
         mainView.addSubview(mainLineView)
         mainLineView.snp.makeConstraints {
-            $0.top.equalTo(mainView)
+            $0.top.equalTo(mainView).offset(-300)
             $0.leading.equalToSuperview().offset(margins * 3)
             $0.width.equalTo(60)
-            $0.height.equalTo(UIScreen.main.bounds.height)
+            $0.height.equalTo(UIScreen.main.bounds.height + 300)
         }
         
         mainView.addSubview(mainCircleBgView)
@@ -367,7 +367,7 @@ extension MainViewController: UICollectionViewDataSource {
         if collectionView == thirdBadgeCollectionView {
             return badges.count
         } else {
-           return badges.count
+            return recordData.count
         }
     }
     
@@ -379,7 +379,7 @@ extension MainViewController: UICollectionViewDataSource {
             return cell
         case recentBadgeCollectionVIew:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecentBadgeCell.identifier, for: indexPath) as! RecentBadgeCell
-            cell.setProperties(badge: badges[indexPath.item])
+            cell.setProperties(record: recordData[indexPath.item])
             return cell
         default:
             return UICollectionViewCell()
@@ -389,7 +389,6 @@ extension MainViewController: UICollectionViewDataSource {
 
 extension MainViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print("[Log] offset :", scrollView.contentOffset.y)
         moveAnimate(scrollView.contentOffset.y)
     }
 }
