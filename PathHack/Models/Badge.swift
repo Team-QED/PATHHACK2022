ca7af3e76b211769b401d0c4b98337932af178e1
UPs <42921982+Up-s@users.noreply.github.com>
2022-04-29 21:25:08 +0900

    ADD

---
+++ PathHack/Models/Badge.swift
@@ -0,0 +1,14 @@
+//
+//  Badge.swift
+//  PathHack
+//
+//  Created by Lee on 2022/04/29.
+//
+
+import Foundation
+
+struct Badge: Hashable {
+    let name: String
+    let percent: Int
+    let imageName: String
+}
